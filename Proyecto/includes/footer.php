9cee8b44f6100b96f61796088a53f391f05907b6
Pachonn <109617481+Pachonn@users.noreply.github.com>
2023-11-21 02:29:04 -0600

    Add files via upload

---
+++ Proyecto/includes/footer.php
@@ -0,0 +1,6 @@
+
+<footer class="bg-secondary text-center text-lg-white">
+    <p>Mejia Santa Ana Israel</p> 
+    <p>Romero Ordoñes Brandon Tonatiu</p> 
+    <p>Sanchez Piña Guillermo</p> 
+</footer> 
---
+++ Proyecto/index.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/resources/css/estilos.css">
+</head>
+<body>
+    <div>
+    <?php
+        require_once 'includes/header.php';
+    ?>
+    </div>
+    <footer>
+    <?php
+    include 'includes/footer.php';
+    ?>
+    </footer>
+    </body>
+</html>
+
+
