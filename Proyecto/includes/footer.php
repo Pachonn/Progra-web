43b55c6cac3b914e22e0f62978cdda54a7a742ef
Pachonn <109617481+Pachonn@users.noreply.github.com>
2023-11-21 02:28:08 -0600

    Delete Proyecto directory

--- Proyecto/includes/footer.php
+++
@@ -1,6 +0,0 @@
-
-<footer class="bg-secondary text-center text-lg-white">
-    <p>Mejia Santa Ana Israel</p> 
-    <p>Romero Ordoñes Brandon Tonatiu</p> 
-    <p>Sanchez Piña Guillermo</p> 
-</footer> 
--- Proyecto/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Cerrar sesión
-session_destroy();
-
-// Redirigir a la página index
-header("Location: index.php");
-exit();
-?>
